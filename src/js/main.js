aaa10d101246fd0080627900bae308fa02c01cf1
Adrian Praja <adrian@databank.ro>
2019-06-07 14:26:31 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 
+Ractive.components.switch = Ractive.extend({
+	isolated: true,
+	template: `
+    <div class="dbk-switch">
+	    <label>
+    	  <input type="checkbox" checked="{{value}}">
+     		<span class="switchbar"></span>
+  	  </label>
+  	</div>  
+  `
+});
